f0984d71d66a94f400ad796fcc90460f3214d16a
RivaldoRA <rivaldo6@live.com.mx>
2025-12-03 08:46:17 -0600

    avance: Servidor web

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import puerta
+import servidorWeb
 
 if __name__ == '__main__':
     print ('Program is starting...')
     try:
         puerta.moverMotor()
+        servidorWeb.prenderServidor()
     except KeyboardInterrupt:
         print("Ending program")
